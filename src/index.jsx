e757a5d8e0a9545ca2740bb0b6288a799f280b8f
Hikit9Ipa <Hikit9ipa@gmail.com>
2023-02-06 20:41:55 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-
-
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './components/App/App.jsx';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './utils/store/store';
 import { BrowserRouter} from "react-router-dom";
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
+const root = ReactDOMClient.createRoot(
+document.getElementById('root')
 );
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    </Provider>
+<Provider store={store}>
+<BrowserRouter>
+<App />
+</BrowserRouter>
+</Provider>
 );
 
 reportWebVitals();
